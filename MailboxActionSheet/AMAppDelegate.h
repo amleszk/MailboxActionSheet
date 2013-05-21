5c3b555b5f16da673abdfe12a3e258dcaf1cdb1b
A M Leszkiewicz <amleszk@gmail.com>
2013-05-21 11:25:51 -1000

    Initial Commit

---
+++ MailboxActionSheet/AMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AMAppDelegate.h
+//  MailboxActionSheet
+//
+//  Created by al on 21/05/13.
+//  Copyright (c) 2013 amleszk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MailboxActionSheet/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MailboxActionSheet
+//
+//  Created by al on 21/05/13.
+//  Copyright (c) 2013 amleszk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AMAppDelegate class]));
+    }
+}
